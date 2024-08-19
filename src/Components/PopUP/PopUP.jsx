4991c14fad7ada70b40333e0e52445f902e2d7c1
M7md-gamal33 <m7md.gamal3333@gmail.com>
2024-08-19 12:40:29 +0300

    tmam

--- src/Components/PopUP/PopUP.jsx
+++ src/Components/PopUP/PopUP.jsx
@@ -1,7 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Style from './PopUP.module.css'
 import { HomeContext } from '../../HomeContext/HomeContext'
 import AnnualImg from '../../Images/Screenshot 2024-06-03 065327.png'
+
+import format from 'date-fns/format';
+import { addDays } from 'date-fns';
+import { differenceInDays  } from 'date-fns';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calendar, DateRange } from 'react-date-range';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function PopUp() {
 
@@ -14,21 +23,91 @@ export default function PopUp() {
   function handleBoxClick(event) {
     event.stopPropagation(); // منع إغلاق النافذة عند النقر داخل الـ box
   }
+
+
+  // const refOne = useRef(null);
+  // function handleDocumentClick(event) {
+  //   if (refOne.current && !refOne.current.contains(event.target)) {
+  //     setOpenCalendar(false);
+  //   }
+  // }
+
+  // document.addEventListener('mousedown', handleDocumentClick);
+
+  //! من ضمن ال dateRange------------ 
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date() , 1),
+      key: 'selection'
+    }
+  ]);
+
+  const formatDate = (date) => {
+    return format(date, 'EEEE dd MMMM yyyy');
+  };
   
+  const [openCalendar, setOpenCalendar] = useState(false)
+
+  function showCalendar(){
+    setOpenCalendar(!openCalendar);
+  }
+
+   //! من ضمن ال dateRange------------ 
+
+   const handleOutsideClick = (e) => {
+    // Check if the click is outside the calendar and input
+    if (
+      openCalendar &&
+      !e.target.closest('.custom-date-range') &&
+      !e.target.closest('.input-group-annual')
+    ) {
+      setOpenCalendar(false);
+    }
+  };
+
+  // Attach click listener to handle clicks outside input and calendar
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [openCalendar]);
+
+  //العـــــــوده
+
+   // dateRange دالة لحساب الفرق في الأيام ل 
+   const calculateDays = () => {
+    const start = range[0].startDate;
+    const end = range[0].endDate;
+    return differenceInDays(end, start) + 1; // +1 لتضمين كل من تاريخ البداية والنهاية
+  };
+
+
+  // ReactQuill تبع ال .........................
+  const [value, setValue] = useState('');
+    
+
+  const module = {
+  toolbar: [
+      ['bold', 'italic' ,{ list: "ordered" }, { list: "bullet" } , "image" , "clean"]
+  ]
+  }
+  // ReactQuill تبع ال ........................
 
   return <>
 
   {ShowDialog? 
 
     <section onClick={ close} className="light-box">
-        <div onClick={ handleBoxClick} className="box-data rounded-1 shadowBox">
+        <div onClick={ handleBoxClick } className="box-data rounded-1 shadowBox">
 
           <div className="under-line100 d-flex justify-content-between p-3">
             <h6>Annual Leave Request</h6>
             <i onClick={close} className="pointer first-number-color fa-solid fa-xmark"></i>
           </div>
 
-          <div className="row g-2">
+          <div className="row g-0">
             <div className="col-md-8">
               <div className="pt-3 px-5">
 
@@ -45,13 +124,34 @@ export default function PopUp() {
 
 
                   <div className="d-flex justify-content-between align-items-center first-popup-word pt-3 ">
-                    <p className='mb-0 pb-1'>Date \ Period</p>
-                    <p className='mb-0 pb-1'>Period : <span className="first-number-color fw-medium">14 Days</span></p>
+                    <p className='text-dark fw-medium mb-0 pb-1'>Date \ Period</p>
+                    <p className='mb-0 pb-1'>Period : <span className="first-number-color fw-medium">{calculateDays()} Days</span></p>
                   </div>
 
-                  <div className="">
-                    
+
+
+                  <div onClick={()=>{showCalendar()}} className='input-group-annual d-flex justify-content-end align-items-center'>
+                    <input readOnly value={`${formatDate(range[0].startDate , "MM/dd/yyyy")}  -  ${formatDate(range[0].endDate , "MM/dd/yyyy")}`} className="w-100 rounded-4 p-3"   placeholder="Annual Leave" ></input>
+                    <i className="colorOfCal pe-3 fa-solid fa-calendar-days"></i> 
                   </div>
+                  <div >
+                  {openCalendar? <DateRange
+                    editableDateInputs={true}
+                    onChange={item => setRange([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={range}
+                    months={2}
+                    direction='horizontal'
+                    minDate={new Date()}
+                    className='w-100 custom-date-range'
+                  />:<> <div className="first-popup-word pt-3 ">
+                          <p className='text-dark fw-medium mb-0 pb-1'>Comment</p>
+                        </div>
+              
+                         <ReactQuill modules={module} className="custom-quill-editor custom-quill-toolbar w-100" theme="snow" value={value} onChange={setValue} />
+                      </>}
+                  </div>
+                  
                 </form>
 
               </div>
@@ -65,6 +165,10 @@ export default function PopUp() {
 
           </div>
           
+          {!openCalendar? <div className="uper-line1 btns-of-annualRequest d-flex justify-content-end align-items-center">
+                    <button onClick={ close} className="btn1 mt-2 me-3 first-number-color">Cancel</button>
+                    <button className="btn2 mt-2 me-4 text-white">Send</button>
+                </div>: ""}
 
         </div>
     </section>
